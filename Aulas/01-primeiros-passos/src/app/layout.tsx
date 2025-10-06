95b3f901137f9788f3ed7d91297bc3a27b005d7e
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-10-06 19:00:51 -0300

    add

--- Aulas/01-primeiros-passos/src/app/layout.tsx
+++ Aulas/01-primeiros-passos/src/app/layout.tsx
@@ -1,5 +1,21 @@
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
+import localFont from "next/font/local"
+import "./globals.css"
+import {Quicksand} from "next/font/google"
+
+const robotoCondensed = localFont({
+  src: "./fonts/robotoCondensed-VariableFont_wght.ttf",
+  variable: "--font-roboto-condensed",
+  weight: '400 700',
+  display: "swap",
+})
+
+const QuicksandFonts = Quicksand({
+  variable: '--font-quicksand',
+  weight: '300',
+
+})
 
 export default function RootLayout({
   children,
@@ -8,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${robotoCondensed.variable} ${QuicksandFonts.variable}`}>
         <Theme appearance="dark">
           {children}
         </Theme>
